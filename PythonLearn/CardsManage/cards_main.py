f37a28b2d7437cec8905a0ce6734cc1ef78a6979
张建通 <jtzhang163@163.com>
2018-09-23 17:36:27 +0800

    判断输入

--- PythonLearn/CardsManage/cards_main.py
+++ PythonLearn/CardsManage/cards_main.py
@@ -1,1 +1,14 @@
 
+action_str = input("请选择希望执行的操作：")
+print("您选择的操作是【%s】" % action_str)
+
+#1,2,3 针对名片的操作
+if action_str in ["1","2","3"]:
+    pass
+
+#0 退出系统
+elif action_str == "0":
+    pass
+
+else:
+    print("您输入的不正确，请重新输入")
