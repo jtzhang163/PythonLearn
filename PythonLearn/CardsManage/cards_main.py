833b2d8384560f70a4a81e65fb1f42829cbd6fb3
张建通 <jtzhang163@163.com>
2018-09-23 18:16:04 +0800

    欢迎界面

--- PythonLearn/CardsManage/cards_main.py
+++ PythonLearn/CardsManage/cards_main.py
@@ -1,18 +1,35 @@
 
+import cards_tools
+
 while True:
+    
+    #显示功能菜单
+    cards_tools.show_menu()
 
     action_str = input("请选择希望执行的操作：")
     print("您选择的操作是【%s】" % action_str)
 
     #1,2,3 针对名片的操作
     if action_str in ["1","2","3"]:
+
+        #新建名片
+        if action_str == "1":
+            pass
+
+        #显示全部
+        elif action_str == "2":
+            pass
+
+        #查询名片
+        else:
+            pass
+
         pass
 
     #0 退出系统
     elif action_str == "0":
         print("欢迎再次使用【名片管理系统】")
         break
-        pass
 
     else:
         print("您输入的不正确，请重新输入")
