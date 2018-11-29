050d07b19c5704a9b64fc1dfe0012067b88e0c74
张建通 <jtzhang163@163.com>
2018-11-29 10:26:58 +0800

    发送各种字符

--- Network/UdpSocket.py
+++ Network/UdpSocket.py
@@ -3,9 +3,15 @@ import socket
 def main():
     # 创建了一个UDP套接字
     udp_socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM)
+
+    # 从键盘获取数据
+    send_data = input("请输入要发送的数据：")
+
     # 使用套接字收发数据
-    udp_socket.sendto(b"hahaha",("192.168.146.1",8080))
+    udp_socket.sendto(send_data.encode("utf-8"),("192.168.146.1",8080))
+
     # 关闭套接字    
     udp_socket.close()
+
 if __name__ == "_main_":
     main()
