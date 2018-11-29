0959d0112a2240e99560e57c151989c1185d73c3
张建通 <jtzhang163@163.com>
2018-11-29 17:00:27 +0800

    udp接收

---
+++ Network/UdpReceive.py
@@ -0,0 +1,22 @@
+import socket
+
+def main():
+
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    udp_socket.bind(("",7788))
+
+    while True:
+
+        recv_data = udp_socket.recvfrom(1024)
+
+        recv_msg = recv_data[0]
+
+        send_addr = recv_data[1]
+
+        print("发送方：%s \r\n接收到的数据: %s" % (str(send_addr), recv_msg.decode("gbk")))
+
+    udp_socket.close()
+
+if __name__ == "__main__":
+    main()
