a5a98b5009085bb6ed0a1efd93624c24246e8c5e
张建通 <jtzhang163@163.com>
2018-12-03 22:22:09 +0800

    下载文件客户端

---
+++ Network/TCP/FileDownloadClient.py
@@ -0,0 +1,22 @@
+import socket
+
+def main():
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    dest_ip = input("服务器IP：")
+    dest_port = int(input("服务器Port："))
+
+    tcp_socket.connect((dest_ip, dest_port))
+
+    file_name = input("要下载的文件名：")
+
+    tcp_socket.send(file_name.encode("utf-8"))
+
+    recv_data = tcp_socket.recv(1024)  # 1k大小
+
+    with open("[附件]"+ file_name, "wb") as f:  # 
+        f.write(recv_data)
+
+
+if __name__ == "__main__":
+    main()
