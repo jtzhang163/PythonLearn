ce26b6f527f69e392dc5487997019343ae29eaaf
张建通 <jtzhang163@163.com>
2018-12-05 15:06:48 +0800

    资源竞争

---
+++ Thread/Competition4Resources.py
@@ -0,0 +1,40 @@
+import threading
+import time
+
+
+g_num = 0
+
+
+def test1(num):
+    global g_num
+    for i in range(num):
+        g_num += 1
+
+    print("------in test1 g_num = %d----" % g_num)
+
+
+def test2(num):
+    global g_num
+    for i in range(num):
+        g_num += 1
+
+    print("------in test2 g_num = %d----" % g_num)
+
+
+def main():
+    t1 = threading.Thread(target=test1, args=(100000,))
+    t2 = threading.Thread(target=test2, args=(50000,))
+
+    t1.start()
+    # time.sleep(1)
+
+    t2.start()
+    # time.sleep(1)
+
+    time.sleep(5)
+
+    print("------in main g_num = %d----" % g_num)
+
+
+if __name__ == "__main__":
+    main()
