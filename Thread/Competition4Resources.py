6ea78c979157631b4515a3cf8e1cbe9ba1b9ed95
张建通 <jtzhang163@163.com>
2018-12-05 15:24:22 +0800

    用互斥锁解决资源竞争问题

--- Thread/Competition4Resources.py
+++ Thread/Competition4Resources.py
@@ -7,23 +7,34 @@ g_num = 0
 
 def test1(num):
     global g_num
+
+    mutex.acquire()
+
     for i in range(num):
         g_num += 1
 
     print("------in test1 g_num = %d----" % g_num)
-
+    
+    mutex.release()
 
 def test2(num):
     global g_num
+
+    mutex.acquire()
+
     for i in range(num):
         g_num += 1
 
     print("------in test2 g_num = %d----" % g_num)
 
+    mutex.release()
+
+mutex = threading.Lock()  # 创建一个互斥锁
+
 
 def main():
-    t1 = threading.Thread(target=test1, args=(100000,))
-    t2 = threading.Thread(target=test2, args=(50000,))
+    t1 = threading.Thread(target=test1, args=(1000000,))
+    t2 = threading.Thread(target=test2, args=(500000,))
 
     t1.start()
     # time.sleep(1)
