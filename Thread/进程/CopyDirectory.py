0d3e8109ede27b4e2ee831578e673dbd7d160050
张建通 <jtzhang163@163.com>
2018-12-07 11:21:33 +0800

    复制文件夹优化

--- Thread/进程/CopyDirectory.py
+++ Thread/进程/CopyDirectory.py
@@ -1,9 +1,9 @@
 import os
 import multiprocessing
 
-def copy_file(file_name, old_folder_name, new_folder_name):
+def copy_file(q,file_name, old_folder_name, new_folder_name):
     """cooy file"""
-    print("from: %s to: %s file: %s " % (old_folder_name, new_folder_name, file_name))
+    # print("from: %s to: %s file: %s " % (old_folder_name, new_folder_name, file_name))
     
     old_file = open(old_folder_name + "/" + file_name, "rb")
     content = old_file.read()
@@ -11,11 +11,11 @@ def copy_file(file_name, old_folder_name, new_folder_name):
     new_file = open(new_folder_name + "/" + file_name, "wb")
     new_file.write(content)
     new_file.close()
-
+    q.put(file_name)
 
 
 def main():
-    # 
+     
     old_folder_name = input("input old folder name:")
     new_folder_name = old_folder_name + "_copy"
     try:
@@ -26,11 +26,25 @@ def main():
 
     po = multiprocessing.Pool(5)
 
+    q = multiprocessing.Manager().Queue()
+
     for file_name in file_names:
-        po.apply_async(copy_file, args=(file_name,old_folder_name,new_folder_name))
+        po.apply_async(copy_file, args=(q,file_name,old_folder_name,new_folder_name))
     
     po.close()
-    po.join()
+
+    all_num = len(file_names)
+    ok_num = 0
+
+    while True:
+        file_name = q.get()
+        ok_num += 1
+        print("\r%.2f%% : copy finish : %s ..." % (ok_num * 100/ all_num, file_name), end='')
+        if ok_num >= all_num:
+            break
+    
+    print("")
+    # po.join()
 
 
 
