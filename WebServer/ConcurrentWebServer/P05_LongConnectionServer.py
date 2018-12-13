d63facc36a22501603dd6a8434eb2bb78e5c480b
张建通 <jtzhang163@163.com>
2018-12-13 10:18:55 +0800

    长连接

--- WebServer/ConcurrentWebServer/P05_LongConnectionServer.py
+++ WebServer/ConcurrentWebServer/P05_LongConnectionServer.py
@@ -1,41 +1,66 @@
 import socket
-import time
+import re
+
+def service_client(new_socket, request):
+
+    request_lines = request.splitlines()
+
+    # GET /Index.html HTTP/1.1
+    ret = re.match(r"[^/]+(/[^ ]*)", request_lines[0])
+    if ret:
+        file_name = ret.group(1)
+        if file_name == "/":
+            file_name = "/index.html"
+
+    try:
+        f = open("./html" + file_name, "rb")
+    except:
+        response_header = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+        response_body = ("<h1>file not found: " + file_name + "<h1>").encode("utf-8")
+    else:
+        response_body = f.read()  # 注意 response_body 是 bytes 类型
+        f.close()
+        response_header = "HTTP/1.1 200 OK\r\n"
+        response_header += "Content-Length:%d" % len(response_body)
+        response_header += "\r\n"
+
+    new_socket.send(response_header.encode("utf-8"))
+    new_socket.send(response_body)
+    new_socket.close()
+
 
 def main():
 
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_server_socket.bind(("", 7890))
-    tcp_server_socket.setblocking(False)  # 设置为非阻塞
+    tcp_server_socket.setblocking(False)
     tcp_server_socket.listen(128)
 
     client_sockets = list()
 
     while True:
-
-        time.sleep(1)
-
         try:
-            new_client, client_addr = tcp_server_socket.accept()
+            new_socket, addr_data = tcp_server_socket.accept()
         except Exception as ex:
-            print("—————没有新的客户端连接—————")
+            pass
         else:
-            new_client.setblocking(False)
-            client_sockets.append(new_client)
+            client_sockets.append(new_socket)
+            new_socket.setblocking(False)
 
         for client_socket in client_sockets:
             try:
                 recv_data = client_socket.recv(1024)
-            except Exception as ex:
-                print(ex)
-                print("—————这个客户端没有发送过来数据—————")
+            except Exception as ex2:
+                pass
             else:
                 if recv_data:
-                    print("—————客户端发送过来了数据>>>", recv_data.decode("utf-8"))
-                else:   
-                    client_sockets.remove(client_socket)
+                    request = recv_data.decode("utf-8")
+                    service_client(client_socket, request)
+                else:
                     client_socket.close()
-                    print("—————客户端已经关闭—————")
+                    client_sockets.remove(client_socket)
 
+    tcp_server_socket.close()
 
 if __name__ == "__main__":
     main()
